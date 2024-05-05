48627c00409f81b6ee30763071f23e867dbabf1d
Celso Gando <Celsogando7@gmail.com>
2024-05-06 00:31:26 +0200

    Updated

--- models/Taskmodel.js
+++ models/Taskmodel.js
@@ -1,28 +1,38 @@
 const { Schema, model } = require('mongoose')
 
 // TODO: Please make sure you edit the Book model to whatever makes sense in this case
-const bookSchema = new Schema(
+const taskSchema = new Schema(
   {
     title: {
       type: String,
-      required: [true, 'Title is required.'],
+      required: [true, 'Task title is required.'],
       trim: true,
     },
-    author: {
+    description: {
       type: String,
-      required: [true, 'Author is required.'],
+      required: [true, 'Task description is required.'],
       trim: true,
     },
-    pages: {
-      type: Number,
-      required: [true, 'Pages is required.'],
+    Priority: {
+      type: String,
+      enum: ['Low', 'Medium', 'High'],
+      required: [true, 'Task priority is required.'],
     },
-  },
+    dueDate: {
+      type: Date,
+      required: [true, 'Due date is required.'],
+      trim: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+  }, 
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
     timestamps: true,
   }
-)
+);
 
 const Book = model('Book', bookSchema)
 
